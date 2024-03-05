c91488421e02a2c3bb28d90b10e859a8862ea1d0
jsneham <sneha.jadhav2402@gmail.com>
2024-03-06 01:52:32 +0530

    added material ui

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const AppLayout = () =>{
+
+    return (
+        <div>123</div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>)
